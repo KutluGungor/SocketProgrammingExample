949ea88f488ae5a0000c1a43fb2a031388353f90
kutlu <kutlugungor58@hotmail.com>
2020-01-06 23:08:31 +0300

    Project Completed.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Timers;
+
+namespace ClientApp
+{
+    class Program
+    {
+        private static Timer _timerTwentySeconds;
+        private static TcpClient client;
+        private static NetworkStream nwStream;
+        static void Main(string[] args)
+        {
+            //Timer tanımlaması yapılır.
+            _timerTwentySeconds = new Timer(20000);
+            _timerTwentySeconds.Elapsed += new ElapsedEventHandler(StateTwentySecond);
+            _timerTwentySeconds.Start();
+
+            Console.ReadLine();
+            client.Close();
+        }
+
+        private static void StateTwentySecond(object sender, ElapsedEventArgs e)
+        {
+            string textToSend = "DATETIME";
+            
+            //Bu Ip üzerindeki portta Client oluşturulur.
+            client = new TcpClient("127.0.0.1", 8000);
+            nwStream = client.GetStream();
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+            
+
+            Console.WriteLine("Gönderilen Bilgi : " + textToSend);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+            
+            //Serverdan dönen değer okunur.
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            var received = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+            if (received == DateTime.Now.ToString())
+            {
+                Console.WriteLine("Sunucu ile zamanımız aynı.");
+            }
+            else
+            {
+                Console.WriteLine("Sunucu ile zamanımız farklı");
+            }
+
+            Console.WriteLine("Sunucudan Gelen Bilgi : " + received);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerApp
+{
+    class Program
+    {
+        private static TcpListener listener;
+        static void Main(string[] args)
+        {
+            //Belirtilen Ip adresi üzerindeki port dinlenmeye başlanır.
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8000);
+            Console.WriteLine("Dinleme Başlatıldı...");
+            Console.WriteLine();
+            listener.Start();
+
+            while (true)
+            {
+                
+                //Client'tan gelecek istek kabul edilir.
+                TcpClient client = listener.AcceptTcpClient();
+                
+                //Network üzerinden gelen data alınır.
+                NetworkStream nwStream = client.GetStream();
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+                
+                //Gelen data okunur.
+                int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+                
+                //Datanın string türüne çevrilmesi gerçekleştirilir.
+                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+
+                if (dataReceived == "DATETIME")
+                {
+                    Console.WriteLine("Gelen Bilgi : " + dataReceived);
+
+                    //Client'a data gönderilir.
+                    byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(DateTime.Now.ToString());
+                    Console.WriteLine("İstemciye Gönderilen Bilgi : " + DateTime.Now);
+                    Console.WriteLine("");
+                    nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                }
+                client.Close();
+            }
+
+            listener.Stop();
+            Console.ReadLine();
+        }
+    }
+}
